92a2216693a341353bf07a185d0ceb98d492d7b5
Andrew Stevenson <andrew@datamountaineer.com>
2015-03-06 09:55:19 +0100

    removed println

--- src/main/resources/sqoop-runner.sh
+++ src/main/resources/sqoop-runner.sh
@@ -6,18 +6,20 @@ db_type=$2
 server=$3
 database=$4
 
+base=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
 export INGESTOR_CLASS="com.datamountaineer.sqoop.sqoop.ingestor"
 export INITIALISER_CLASS="com.datamountaineer.sqoop.sqoop.initialiser"
-export SQOOP_SERVICE_JAR=sqoop-service-assembly-0.1.jar
+export SQOOP_SERVICE_JAR=${base}/../sqoop-service-assembly-0.1.jar
 export SQOOP_HOME=/usr/lib/sqoop
-export HADOOP_CLASSPATH=conf/sqoop-site.xml
+export HADOOP_CLASSPATH=${base}/conf/sqoop-site.xml
 export HADOOP_HOME=/usr/lib/hadoop
 export HADOOP_MAPRED_HOME=/usr/lib/hadoop-mapreduce
 export LIBJARS=${SQOOP_HOME}/*:${SQOOP_HOME}/lib/*
-
-#put sqoop_service_jar first otherwise slick spits alot of debug crap out. Picks the log level up from zookeeper.
 export JAR_CLASSPATH=${SQOOP_SERVICE_JAR}:${LIBJARS}:${HADOOP_HOME}/*:${HADOOP_HOME}/lib/*
 
+JAVA_OPTS_CONF=-Dconfig.file=${base}/../conf/application.conf
+JAVA_OPTS_LOG=-Dlogger.resource=${base}../conf/logback.xml
 
 if [[ "${type}" == "create" || "${type}" == "exec" ]]
 then
@@ -26,5 +28,5 @@ then
  elif [[ "${type}" == "initialise" ]]
  then
     echo "Initialising ${server}/${database}"
-    java -cp ${JAR_CLASSPATH} ${INITIALISER_CLASS} ${db_type} ${server} ${database}
+    java ${JAVA_OPTS_CONF} ${JAVA_OPTS_LOG} -cp ${JAR_CLASSPATH} ${INITIALISER_CLASS} ${db_type} ${server} ${database}
  fi
