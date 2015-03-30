b0a634ebbc84680c32ea5e1613c23cf225e09e3c
Andrew Stevenson <andrew@datamountaineer.com>
2015-03-30 09:24:12 +0200

    refactor build.sbt and stuff

--- post_deploy.sh
+++ post_deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+
+
 sql_ddl="CREATE DATABASE sqoop_metastore;GRANT CREATE, UPDATE, DELETE, SELECT, ALTER, INDEX on sqoop_metastore.* to 'sqoop'@'localhost' identified by 'sqoop'"
 e
-echo $sql_ddl | mysql -u$1 -p
+echo ${sql_ddl} | mysql -u$1 -p
 
